8684a4fd410f9e7f58f4de390c1066da755f5bee
youssef lab20-u <yousseflab20@gmail.com>
2025-11-05 12:01:40 +0100

    add your liste atrractions for page

--- app/(tabs)/ListOfAttractions.tsx
+++ app/(tabs)/ListOfAttractions.tsx
@@ -1,15 +1,18 @@
+import { AttractionsSelector } from '@/service/AttractionsSelector';
 import Feather from '@expo/vector-icons/Feather';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import Ionicons from '@expo/vector-icons/Ionicons';
 // import { TextInput } from 'react-native-paper';
-import { TextInput } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native';
 // Icone
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 export default function ListOfAttractions(){
+const tabHeight = useBottomTabBarHeight();
+
     return(
         <SafeAreaView style={{backgroundColor:"#f1ece1ff",flex:1}}>
-        <ScrollView>
             
                 <View style={styles.content}>
                 <View style={styles.containerIcone}>
@@ -20,11 +23,37 @@ export default function ListOfAttractions(){
                     <View style={styles.containerRecherche}>
                     <Feather name="search" size={24} color="black"/>
                 <TextInput placeholder='search' style={{textAlign:"center",flex:1,marginLeft:5}}/>
-                
                 </View>
+                <View style={styles.containerButton}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.Text1}>All</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.Text1}>Nature</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.Text1}>History</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.Text1}>Culture</Text>
+                    </TouchableOpacity>
+                </View>
+                <FlatList 
+                data={AttractionsSelector}
+                keyExtractor={(item)=>item.id.toString()}
+                contentContainerStyle={{  paddingBottom: tabHeight + 140}}
+                renderItem={({item})=>(
+                    <View style={styles.Card}>
+                        <Image style={styles.image}source={item.image} resizeMode="cover"/>
+                    <View style={{alignSelf:'flex-start',marginTop:5,paddingHorizontal:5}}>
+                        <Text style={styles.Text2}>{item.title}</Text>
+                    <Text style={styles.description}>{item.description}</Text>
+                    </View>
+                    </View>
+                )}
+                />
             </View>
             
-</ScrollView>
 </SafeAreaView>
     )
 }
@@ -51,8 +80,48 @@ const styles = StyleSheet.create({
        flexDirection: "row",
         justifyContent: 'center',
         alignItems: "center",
-        borderRadius:10, 
+        borderRadius:10,
         paddingHorizontal:10,
         marginTop:20
+    },
+    containerButton:{
+        marginTop:17,
+        alignItems:"center",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginBottom:10
+    },
+    button:{
+        backgroundColor:"#f1cbbaff",
+        width:85,
+        padding:17,
+        borderRadius:50,
+    },
+    Text1:{
+        textAlign:"center",
+        fontWeight:"bold",
+    },
+    Card:{
+        backgroundColor:"#fff",
+        alignItems:"center",
+        padding:10,
+        marginVertical:10,
+        borderRadius:15,
+        shadowColor:"red",
+        shadowOffset:{width:0,height:2},
+        shadowOpacity:0.5,
+        shadowRadius:4
+    },
+    image:{
+        height:180,
+        width:"95%",
+        borderRadius:15
+    },
+    Text2:{
+        fontWeight:"bold",
+        fontSize:17,
+    },
+    description:{
+        marginTop:3
     }
 })
---
+++ service/AttractionsSelector.tsx
@@ -0,0 +1,26 @@
+export const AttractionsSelector=[
+    {
+        id:1,
+        image:require('../assets/images/BeniMelal.jpg'),
+        title:"Cascades D'Ouzoud ",
+        description:"Morocco's most spectacular waterfalls",
+    },
+    {
+        id:2,
+        image:require('../assets/images/Bin El Ouidane Lake.jpg'),
+        title:"Bin El Ouidane Lake",
+        description:'A serene reservoir in the Atlas Mountains',
+    },
+    {
+        id:3,
+        image:require('../assets/images/Cascades DOuzoud.jpg'),
+        title:"Kasbah Ras El Ain",
+        description:'Historic fortress overlooking the city',
+    },
+    {
+        id:4,
+        image:require('../assets/images/Beni Mellal Souk.jpg'),
+        title:"Beni Mellal Souk",
+        description:'Experience the vibrant traditional market',
+    }
+]
